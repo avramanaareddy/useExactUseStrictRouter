6acbffd9d88cec9c04671cad05fbb3fd94ad11bb
Vennkata <avramanaareddy@yahoo.com>
2020-10-22 15:12:56 -0400

    use strict implementation done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
 import "./styles.css";
-
+import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+        <Route
+          path="/" exact
+          render={() => {
+            return <div> This is Home page</div>;
+          }}
+        />
+{/**
+  When you use exact strict, in the browser 
+  if you give the url path as /about, then it 
+  is not going to work, you must provide the 
+  /about/ to execute the path
+ */}
+        <Route
+          path="/about/" exact strict
+          render={() => {
+            return <div> This is About </div>;
+          }}
+        />
+        </Switch>
+      </div>
+    </Router>
   );
 }
